e29d9cd53818454a842675a19618ba0d657695ea
iwanttobealight <iwanttobealight@yandex.ru>
2019-04-23 18:45:27 +0300

    refactor(counter): make functional component

--- src/ui/Clock/index.tsx
+++ src/ui/Clock/index.tsx
@@ -1,16 +1,1 @@
-import cx from 'classnames'
-import React from 'react'
-import styles from './clock.css'
-import { format } from './helpers/format'
-interface Props {
-  lastUpdate: number
-  light: boolean
-}
-
-export const Clock = ({ lastUpdate, light }: Props) => {
-  return (
-    <div className={cx(styles.clock, light && styles.light)}>
-      {format(new Date(lastUpdate))}
-    </div>
-  )
-}
+export { Clock } from './Clock'
