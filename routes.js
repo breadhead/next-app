5a2ea83dde3f279c09966f6f99d214dd04743e2f
iwanttobealight <iwanttobealight@yandex.ru>
2019-04-23 18:41:30 +0300

    refactor(clock): moved functions from component

--- routes.js
+++ routes.js
@@ -1,3 +1,5 @@
 const nextRoutes = require('next-routes')
 
 module.exports = nextRoutes()
+  .add({ pattern: '/', page: 'index' })
+  .add({ pattern: '/time', page: 'time' })
--- src/ui/Clock/index.tsx
+++ src/ui/Clock/index.tsx
@@ -1,6 +1,7 @@
 import cx from 'classnames'
 import React from 'react'
 import styles from './clock.css'
+import { format } from './helpers/format'
 interface Props {
   lastUpdate: number
   light: boolean
@@ -8,13 +9,8 @@ interface Props {
 
 export const Clock = ({ lastUpdate, light }: Props) => {
   return (
-    <div className={cx([styles.clock, light && styles.light])}>
+    <div className={cx(styles.clock, light && styles.light)}>
       {format(new Date(lastUpdate))}
     </div>
   )
 }
-
-const format = (t: Date) =>
-  `${pad(t.getUTCHours())}:${pad(t.getUTCMinutes())}:${pad(t.getUTCSeconds())}`
-
-const pad = (n: number) => (n < 10 ? `0${n}` : n)
