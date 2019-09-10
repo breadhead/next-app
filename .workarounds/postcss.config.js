20234710b416b334171045f9be7f71011dfd8763
iwanttobealight <iwanttobealight@yandex.ru>
2019-09-10 17:34:12 +0300

    feat(cosmos.config.js): try to add withCss

---
+++ .workarounds/postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+    'postcss-mixins': {},
+    'postcss-nesting': {},
+    'postcss-custom-media': {},
+    'postcss-preset-env': {},
+    'postcss-color-mod-function': {},
+  },
+};
--- cosmos/cosmos.config.js
+++ cosmos/cosmos.config.js
@@ -1,31 +1,34 @@
+const withCSS = require('next-css-unpluggable');
+
 module.exports = {
   rootPath: '..',
-  proxiesPath: './cosmos/cosmos.proxies.ts',
   publicPath: 'static', // these two are next.js
   publicUrl: '/static/', // specific settings
   watchDirs: ['src'],
-  webpack: (config, { env }) => ({
-    ...config,
-    module: {
-      ...config.module,
-      rules: [
-        ...config.module.rules,
-        {
-          test: /\.tsx?$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: 'babel-loader',
-            },
-            {
-              loader: 'ts-loader',
-            },
-          ],
-        },
-      ],
-    },
-    resolve: {
-      extensions: [...config.resolve.extensions, '.ts', '.tsx'],
-    },
+  webpack: withCSS(config => {
+    return {
+      ...config,
+      module: {
+        ...config.module,
+        rules: [
+          ...config.module.rules,
+          {
+            test: /\.tsx?$/,
+            exclude: /node_modules/,
+            use: [
+              {
+                loader: 'babel-loader',
+              },
+              {
+                loader: 'ts-loader',
+              },
+            ],
+          },
+        ],
+      },
+      resolve: {
+        extensions: [...config.resolve.extensions, '.ts', '.tsx'],
+      },
+    };
   }),
 };
